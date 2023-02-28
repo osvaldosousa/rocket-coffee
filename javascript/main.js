b1018530f398b5f35168eaf81d767cb6ea379b33
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-02-28 18:10:34 -0300

    layout desktop finesh

--- javascript/main.js
+++ javascript/main.js
@@ -4,7 +4,7 @@ const navLinks = document.querySelector('.nav-links');
 const intensNav = document.querySelectorAll('.itens-nav');
 
 menuMobile.addEventListener('click', () => {
-  document.body.style.overflow = scrollPage ? "hidden" : "initial"
+  document.body.style.overflow = scrollPage ? "hidden" : "initial";
   menuMobile.classList.toggle('-open');
   navLinks.classList.toggle('-open');
   scrollPage = !scrollPage
